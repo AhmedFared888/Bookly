e91020fe5487f9e9860c7cd9e9ce1c2998b47f7d
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-01-11 18:17:12 -0800

    dd title & subtitle to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/Core/utils/styles.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/Custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +15,37 @@ class BookDetailsViewBody extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const CustomBookDetailsAppBar(),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: width * .16),
                 child: const CustomBookImage(),
               ),
+              const SizedBox(
+                height: 43,
+              ),
+              Text(
+                'The Jungle Book',
+                style: Styles.textStyle30.copyWith(fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text(
+                  'Rudyard Kipling',
+                  style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              const BookRating(),
             ],
           ),
         ),
