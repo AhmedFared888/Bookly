989a9e4fb75bd5aa1bac78e47205ff8101a3cd2e
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-01-12 14:14:53 -0800

    Search result view ui done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/Core/utils/styles.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/Best_Seller_List_View_Item.dart';
 import 'package:bookly/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/Features/search/presentation/views/widgets/search_result_list_view.dart';
+import 'package:bookly/constans.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -6,16 +10,37 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(
-          height: 18,
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30.0),
-          child: CustomSearchTextField(),
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 18,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: CustomSearchTextField(),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 30.0,
+              bottom: 4.0,
+            ),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Search result',
+                style: Styles.textStyle18.copyWith(
+                  fontFamily: kGtSectraFine,
+                ),
+              ),
+            ),
+          ),
+          const SearchResultListView(),
+        ],
+      ),
     );
   }
 }
