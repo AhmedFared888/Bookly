78e16b84a8b494b50d3615a895f0c33549e649f6
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-01-17 22:37:48 -0800

    Display featured books part 1

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:bookly/Core/utils/styles.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/Best_Seller_List_View_Item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featuted_List_view.dart';
